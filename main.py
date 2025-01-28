84a4cc330dd38bea2557cead36a67c111861a16f
unconquerable-space <unconquerable.space@gmail.com>
2025-01-27 19:06:27 -0600

    add player

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
+
+    # initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print(player.position)
+
     while True:
+
+        # event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(pygame.Color(0,0,0))
+
+        # draw and update game clock
+        screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
